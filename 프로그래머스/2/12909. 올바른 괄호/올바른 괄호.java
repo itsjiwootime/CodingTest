dede0289b4ea9d53badef53622f28c65dbcae0bd
itsjiwootime <28gjwldn_@naver.com>
2025-10-21 23:54:59 +0900

    [level 2] Title: 올바른 괄호, Time: 17.60 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        
+        for(char c : s.toCharArray()) {
+            
+            if(c == '('){
+                stack.push(c);
+            } else if(c == ')'){
+                if(stack.isEmpty()){
+                    answer = false;
+                    return answer; 
+                }
+                stack.pop();
+            }
+        }
+        
+        if(!stack.isEmpty()){
+            answer = false;
+        }
+        
+        return answer;
+    }
+}

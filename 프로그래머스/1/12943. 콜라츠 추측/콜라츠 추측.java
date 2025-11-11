e61cd66e635168d74b35e36de97c04c9a8c86b9f
itsjiwootime <28gjwldn_@naver.com>
2025-11-11 23:46:59 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(long num) {
+        int cnt = 0;
+
+        if (num == 1) return 0;
+
+        while (num != 1) {
+            if (cnt == 500) return -1;
+
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num * 3L + 1;  
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+}

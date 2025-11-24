03f6e76d4460af4d2d75b7ae01f71aa5a816cf28
itsjiwootime <28gjwldn_@naver.com>
2025-11-24 23:13:52 +0900

    Create 1이 될 떄까지.java

---
+++ 1이 될 떄까지.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        
+
+        while (N != 1) {
+            if (N % K == 0) {
+                N /= K;
+                cnt++;
+            } else  {
+                N -= 1;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}

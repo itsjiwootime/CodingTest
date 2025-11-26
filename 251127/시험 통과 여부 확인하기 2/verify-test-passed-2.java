12e536ce695e8bdcae6a7491a77c912730aef88f
itsjiwootime <28gjwldn_@naver.com>
2025-11-27 01:38:39 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251127/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] arr = new int[N][4];
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < 4; j++){
+                int num = sc.nextInt();
+                arr[i][j] = num;
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < N; i++){
+            double sum = 0;
+            for(int j = 0; j < 4; j++){
+                
+                sum += arr[i][j];
+            }
+            if(sum / 4 >= 60){
+                System.out.println("pass");
+                cnt++:
+            } else {
+                System.out.println("fail");
+            }
+        }
+        System.out.println(cnt);
+    }
+}

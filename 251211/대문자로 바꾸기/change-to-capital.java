3eaf0f8e3c81694b8a5ba421fadc54d09f322be1
itsjiwootime <28gjwldn_@naver.com>
2025-12-11 00:39:56 +0900

    Title: 대문자로 바꾸기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251211/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] arr = new char[5][3];
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 3; j++) {
+                arr[i][j] = sc.next().charAt(0);
+            }
+        }
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.print(Character.toUpperCase(arr[i][j]) + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}

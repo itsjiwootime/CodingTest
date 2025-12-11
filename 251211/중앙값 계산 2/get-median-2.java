b598516e3ee75eda16841c4ec98e8dffc4129c35
itsjiwootime <28gjwldn_@naver.com>
2025-12-11 15:49:34 +0900

    Title: 중앙값 계산 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251211/중앙값 계산 2/get-median-2.java
+++ 251211/중앙값 계산 2/get-median-2.java
@@ -13,16 +13,27 @@ public class Main {
         int i = 1;
         int j = 0;
 
-        while(true) {
-            if(j == (n/2)+1){
-                break;
-            }
-            Arrays.sort(arr,1,i);
+        int i = 0;       // 실제 읽은 개수 - 1 (0, 2, 4, ...)
+        int printed = 0; // 몇 번 중앙값을 출력했는가
 
-            System.out.print(arr[j] + " ");
+        while (true) {
 
+            if (printed == (n/2 + 1)) break;
+
+            // arr[0..i] 부분을 복사
+            int[] temp = Arrays.copyOfRange(arr, 0, i + 1);
+
+            // 정렬
+            Arrays.sort(temp);
+
+            // 중앙 인덱스
+            int mid = temp.length / 2;
+
+            System.out.print(temp[mid] + " ");
+
+            // 다음 홀수 개수로 이동 (1 → 3 → 5 → …)
             i += 2;
-            j++;
+            printed++;
         }
         
         // Please write your code here.

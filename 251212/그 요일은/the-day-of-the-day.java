1a0af6c1b4662e978b63b29054fab86ccd0cd47c
itsjiwootime <28gjwldn_@naver.com>
2025-12-12 03:24:32 +0900

    Title: 그 요일은, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251212/그 요일은/the-day-of-the-day.java
+++ 251212/그 요일은/the-day-of-the-day.java
@@ -10,7 +10,7 @@ public class Main {
         String A = sc.next();
         // Please write your code here.
 
-        int[] m_day = new int[]{0,31,29,31,30,31,30,31,31,30,31,30,31};
+        int[] mdays = new int[]{0,31,29,31,30,31,30,31,31,30,31,30,31};
         Map<String, Integer> map = new HashMap<>();
         map.put("Mon",0);
         map.put("Tue",1);
@@ -20,29 +20,29 @@ public class Main {
         map.put("Sat",5);
         map.put("Sun",6);
 
-        int day1 = d1;
-        for(int i = 1; i < m1; i++) {
-            day1 += m_day[i];
-        }
-
-        int day2 = d2;
-        for(int i = 1; i < m2; i++) {
-            day2 += m_day[i];
-        }
-
         
+        
+
+         int start = d1;
+        for (int i = 1; i < m1; i++) start += mdays[i];
+
+        int end = d2;
+        for (int i = 1; i < m2; i++) end += mdays[i];
+
+        int diff = end - start;
+
+        int count = 0;
 
         int target = map.get(A);
 
-        int cnt = 0;
+      
 
-        for(int i = day1; i < day2;i++){
-            if(i% 7 == target){
-                cnt++;
-            }
+        for (int i = 0; i <= diff; i++) {
+            int weekday = (i) % 7;
+            if (weekday == target) count++;
         }
 
-        System.out.println(cnt);
+        System.out.println(count);
 
 
 

b6ed4fb718593886a072b86606d51773e6fdc030
itsjiwootime <28gjwldn_@naver.com>
2025-12-11 14:56:39 +0900

    Title: 두 개의 동일한 수열, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251211/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if(a[i] == b[i]){
+                cnt++;
+            }
+        }
+
+        if(cnt == n){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+        
+    }
+}

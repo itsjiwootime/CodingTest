7040e6ca7af82f21945bbf7f06b91a4d17fd1c65
itsjiwootime <28gjwldn_@naver.com>
2025-12-16 23:56:33 +0900

    Title: 블럭쌓는 명령2, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 251216/블럭쌓는 명령2/block-stacking-commands2.java
+++ 251216/블럭쌓는 명령2/block-stacking-commands2.java
@@ -1,34 +1,41 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
+        
 
-        int[] arr = new int[N];
+        int N = sc.nextInt(); 
+        int K = sc.nextInt(); 
+
+     
+        int[] arr = new int[N]; 
+        
+        
         for (int i = 0; i < K; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+            int A = sc.nextInt(); 
+            int B = sc.nextInt(); 
 
+         
             for(int j = A; j <= B; j++) {
+     
                 arr[j-1]++;
             }
         }
 
         int max = 0;
 
-         for (int i = 0; i < K; i++) {
-            
-            if(max < arr[i]){
-                max =arr[i];
+    
+        for (int i = 0; i < N; i++) { 
+           
+            if(arr[i] > max){ 
+                max = arr[i];
             }
+        }
 
-
-         }
-
-         System.out.println(max);
-
-        // Please write your code here.
+        // 최댓값을 출력합니다.
+        System.out.println(max);
         
+      
     }
 }

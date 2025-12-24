b45c1d1d59b0b8d2ab17b062df3a01b08c841a27
itsjiwootime <28gjwldn_@naver.com>
2025-12-25 05:11:10 +0900

    Title: 벌금은 누구에게, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

--- 251225/벌금은 누구에게/who-will-pay.java
+++ 251225/벌금은 누구에게/who-will-pay.java
@@ -8,25 +8,24 @@ public class Main {
         int k = sc.nextInt();
 
         int cnt = -1;
+        int[] student = new int[n + 1];
 
-        int[] student = new int[n+1];
         for (int i = 0; i < m; i++) {
-            int qjfwja = sc.nextInt();
+            int qjfwja = sc.nextInt(); // 벌점 받은 학생 번호 입력
 
-            student[qjfwja]++;
-            if(student[qjfwja] == k) {
-                System.out.println(student[qjfwja]-1);
-                cnt++;
+            student[qjfwja]++; // 해당 학생 벌점 추가
+
+            if (student[qjfwja] == k) {
+                // 수정된 부분: 벌점 개수가 아니라 '학생 번호'를 출력해야 함
+                System.out.println(qjfwja); 
+                cnt = 1; // 찾았다는 표시 (기존 cnt++도 무방합니다)
                 break;
             }
         }
 
-        if(cnt == -1){
+        // 끝까지 m번을 다 돌았는데도 k점에 도달한 사람이 없으면 -1 출력
+        if (cnt == -1) {
             System.out.println(-1);
         }
-        
-
-        
-        // Please write your code here.
     }
 }

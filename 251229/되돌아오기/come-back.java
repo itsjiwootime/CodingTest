ababc495585d1dcda596729e75ab6f55491fc9d7
itsjiwootime <28gjwldn_@naver.com>
2025-12-29 23:57:20 +0900

    Title: 되돌아오기, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

--- 251229/되돌아오기/come-back.java
+++ 251229/되돌아오기/come-back.java
@@ -1,39 +1,44 @@
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.stdin);
-        
+        Scanner sc = new Scanner(System.in);
+
         // 이동 횟수 N 입력
         int n = sc.nextInt();
-        
-        int curX = 0, curY = 0; // 현재 위치
-        int elapsedTime = 0;    // 경과 시간 (초)
+
+        // 현재 위치 (x, y)와 경과 시간
+        int x = 0, y = 0;
+        int elapsedTime = 0;
+
+        // 방향에 따른 좌표 변화량 설정 (W, S, N, E)
+        // W: 서(-x), S: 남(-y), N: 북(+y), E: 동(+x)
+        int[] dx = {-1, 0, 0, 1}; // W, S, N, E 순서
+        int[] dy = {0, -1, 1, 0};
+        String dirs = "WSNE";
 
         for (int i = 0; i < n; i++) {
-            char dir = sc.next().charAt(0); // 방향 (N, S, E, W)
-            int dist = sc.nextInt();        // 이동 거리
+            String dirStr = sc.next();
+            int distance = sc.nextInt();
 
-            // 한 칸씩 이동하며 (0, 0)에 도달하는지 확인
-            for (int j = 0; j < dist; j++) {
-                elapsedTime++; // 1초 경과
+            // 현재 방향의 인덱스 찾기
+            int dirIdx = dirs.indexOf(dirStr);
 
-                if (dir == 'N') curY++;
-                else if (dir == 'S') curY--;
-                else if (dir == 'E') curX++;
-                else if (dir == 'W') curX--;
+            // 한 칸씩 이동하며 (0,0) 체크
+            for (int j = 0; j < distance; j++) {
+                x += dx[dirIdx];
+                y += dy[dirIdx];
+                elapsedTime++;
 
-                // 이동 후 현재 위치가 시작점(0, 0)인지 확인
-                if (curX == 0 && curY == 0) {
+                // 원점으로 돌아왔는지 확인
+                if (x == 0 && y == 0) {
                     System.out.println(elapsedTime);
-                    return; // 처음으로 돌아온 즉시 종료
+                    return; // 프로그램 종료
                 }
             }
         }
 
-        // 모든 이동이 끝날 때까지 돌아오지 못한 경우
+        // 모든 이동 후에도 원점에 도달하지 못한 경우
         System.out.println(-1);
     }
 }

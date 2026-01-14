8b916ccdc19a25048e1d8806af0364ff1ea84d01
itsjiwootime <28gjwldn_@naver.com>
2026-01-14 23:20:19 +0900

    Title: 최고의 33위치, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260114/최고의 33위치/best-place-of-33.java
+++ 260114/최고의 33위치/best-place-of-33.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Main {
+public class Best33Location {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
+    
         int n = sc.nextInt();
         int[][] grid = new int[n][n];
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 grid[i][j] = sc.nextInt();
@@ -15,14 +15,26 @@ public class Main {
 
         int maxCoins = 0;
 
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j + 2 < n; j++) {
-                int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
-                maxCoins = Math.max(maxCoins, sum);
+ 
+        for (int i = 0; i <= n - 3; i++) {
+            for (int j = 0; j <= n - 3; j++) {
+                int currentCoins = countCoins(grid, i, j);
+                maxCoins = Math.max(maxCoins, currentCoins);
             }
         }
 
+ 
         System.out.println(maxCoins);
+    }
+
+
+    private static int countCoins(int[][] grid, int row, int col) {
+        int count = 0;
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                count += grid[i][j];
+            }
+        }
+        return count;
     }
 }

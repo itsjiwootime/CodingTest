1ee0861f1e60ac2094773e0e614e0baa45ab9147
itsjiwootime <28gjwldn_@naver.com>
2026-01-20 23:30:59 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260120/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int dx = x[i] - x[j];
+                int dy = y[i] - y[j];
+                int dist = dx * dx + dy * dy;
+
+                ans = Math.min(ans, dist);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
